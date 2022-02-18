c10d18bad0607e53c33abbff7aa14bb9ca44cafa
Andrei Holub <holubgetrekt@gmail.com>
2022-02-18 15:04:04 +0100

    Script done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   number = 0;
+  pom = 0;
+
+  public test() {
+    this.pom = this.number;
+    if(this.number > 255) {
+      alert("error");
+      this.pom = 0;
+      this.number = 0;
+    }
+    while (this.pom != 0) {
+      if (this.pom >= 128) {
+        this.pom -= 128;
+      }
+      else if (this.pom >= 64) {
+        this.pom -= 64;
+      }
+      else if (this.pom >= 32) {
+        this.pom -= 32;
+      }
+      else if (this.pom >= 16) {
+        this.pom -= 16;
+      }
+      else if (this.pom >= 8) {
+        this.pom -= 8;
+      }
+      else if (this.pom >= 4) {
+        this.pom -= 4;
+      }
+      else if (this.pom >= 2) {
+        this.pom -= 2;
+      }
+      else if (this.pom >= 1) {
+        this.pom -= 1;
+      }
+    }
+  }
 }
