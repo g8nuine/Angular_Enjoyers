9245ebb14f7da0d22d48b268dc2ffe957892c1ea
Danis-s <Shangareev.ds@mail.ru>
2022-02-18 17:35:08 +0100

    only binarization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,62 +9,64 @@ export class AppComponent {
   number = 0;
   pom = 0;
 
-  // Script
+  checkBox1: boolean = false;
+  checkBox2: boolean = false;
+  checkBox3: boolean = false;
+  checkBox4: boolean = false;
+  checkBox5: boolean = false;
+  checkBox6: boolean = false;
+  checkBox7: boolean = false;
+  checkBox8: boolean = false;
+
   public test() {
-    this.pom = this.number;
-    if(this.number > 255) {
+    this.checkBox1 = false;
+    this.checkBox2 = false;
+    this.checkBox3 = false;
+    this.checkBox4 = false;
+    this.checkBox5 = false;
+    this.checkBox6 = false;
+    this.checkBox7 = false;
+    this.checkBox8 = false;
+    let x = (document.getElementById("num") as HTMLInputElement).value;
+    let number = parseInt(x);
+    this.pom = number;
+    if(this.pom > 128) {
       alert("error");
       this.pom = 0;
       this.number = 0;
     }
     while (this.pom != 0) {
-      if (this.pom >= 128) {
+      if (this.pom == 128) {
         this.pom -= 128;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox8 = true;
       }
-      if (this.pom >= 64) {
+      else if (this.pom >= 64) {
         this.pom -= 64;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox7 = true;
       }
-      if (this.pom >= 32) {
+      else if (this.pom >= 32) {
         this.pom -= 32;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox6 = true;
       }
-      if (this.pom >= 16) {
+      else if (this.pom >= 16) {
         this.pom -= 16;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox5 = true;
       }
-      if (this.pom >= 8) {
+      else if (this.pom >= 8) {
         this.pom -= 8;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox4 = true;
       }
-      if (this.pom >= 4) {
+      else if (this.pom >= 4) {
         this.pom -= 4;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox3 = true
       }
-      if (this.pom >= 2) {
+      else if (this.pom >= 2) {
         this.pom -= 2;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox2 = true;
       }
-      if (this.pom >= 1) {
+      else if (this.pom >= 1) {
         this.pom -= 1;
-        //TODO
-      } else {
-        //TODO
+        this.checkBox1 = true;
       }
     }
   }
